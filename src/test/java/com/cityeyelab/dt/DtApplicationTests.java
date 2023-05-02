522078007e0c01b5c876b2b4ec1e2088a2294e9c
IMGIWON <gwim1130@naver.com>
2023-05-02 16:52:19 +0900

    초기세팅

---
+++ src/test/java/com/cityeyelab/dt/DtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityeyelab.dt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
